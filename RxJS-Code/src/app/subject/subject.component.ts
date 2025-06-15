e1a6ede6eb5ddd415f8ac6159c1de4d5aa2353db
Joseph Manning <jgmanning3@yahoo.com>
2025-06-14 20:59:51 -0700

    Create ReplaySubject demo.

--- RxJS-Code/src/app/subject/subject.component.ts
+++ RxJS-Code/src/app/subject/subject.component.ts
@@ -1,16 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, ReplaySubject } from 'rxjs';
 
 @Component({
   selector: 'app-subject',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './subject.component.html',
   styleUrl: './subject.component.css',
 })
 export class SubjectComponent {
   firstCallBS: number = 0;
   secondCallBS: number = 0;
+
+  firstCallReplay: any = [];
+  secondCallReplay: any = [];
 
   startBehaviorCalls() {
     const subject = new BehaviorSubject(0);
@@ -23,7 +27,7 @@ export class SubjectComponent {
     });
 
     setInterval(() => {
-      subject.next(counter += 1);
+      subject.next(counter++);
     }, 1000);
 
     setTimeout(() => {
@@ -36,6 +40,54 @@ export class SubjectComponent {
 
     setTimeout(() => {
       subject.unsubscribe();
-    }, 10000)
+    }, 10000);
+  }
+
+  startReplayCall() {
+    const subject = new ReplaySubject(3);
+    let counter = 0;
+
+    subject.subscribe({
+      next: (x) => {
+        console.log(`observerA: ${x}`), this.firstCallReplay.push(x);
+        console.log(this.firstCallReplay);
+      },
+    });
+
+    subject.next(1);
+    subject.next(2);
+    subject.next(3);
+    subject.next(4);
+
+    setTimeout(() => {
+      subject.subscribe({
+        next: (x) => {
+          console.log(`observerB: ${x}`), this.secondCallReplay.push(x);
+          console.log(this.secondCallReplay);
+        },
+      });
+    }, 4000);
+
+    subject.next(5);
+
+    // setTimeout(() => {
+    //   subject.unsubscribe();
+    // }, 10000);
+    // const subject = new ReplaySubject(3); // buffer 3 values for new subscribers
+
+    // subject.subscribe({
+    //   next: (v) => console.log(`observerA: ${v}`),
+    // });
+
+    // subject.next(1);
+    // subject.next(2);
+    // subject.next(3);
+    // subject.next(4);
+
+    // subject.subscribe({
+    //   next: (v) => console.log(`observerB: ${v}`),
+    // });
+
+    // subject.next(5);
   }
 }
